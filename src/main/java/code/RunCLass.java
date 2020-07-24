bd6ed96aa1e975c1cdf9c62ee0d71628af6e6016
zhaoguanchen <zhaoguanchen@yuanbaobaoxian.com>
2020-07-24 19:26:00 +0800

    init

---
+++ src/main/java/code/RunCLass.java
@@ -0,0 +1,18 @@
+package code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Guanchen Zhao
+ * @Description 启动类
+ * @Date 2020/7/21 11:05
+ **/
+@SpringBootApplication
+public class RunCLass {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunCLass.class, args);
+    }
+
+}
